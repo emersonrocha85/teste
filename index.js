3cc7a2b1a8c18de92a90654a4d2cca96e37da169
Emerson Rocha de Almeida <emersonrocha85@gmail.com>
2021-06-10 22:54:35 -0300

    teste

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const servidor = http.createServer((request, response) => {
+    response.end(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Alô, mundo!</h1>
+            </body>
+        </html>
+    `);
+});
+servidor.listen(3000);
